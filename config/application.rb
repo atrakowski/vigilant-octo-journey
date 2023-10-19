ded738502049e64e85d28d5399c91eb130fe7674
A. Trakowski <3940863+atrakowski@users.noreply.github.com>
2023-10-19 22:48:32 +0200

    Install devise

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module DemoApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Berlin'
+    config.i18n.default_locale = :de
   end
 end
